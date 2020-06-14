031f3bfc7a9d5c1ee34181d0063c70b51521e7bb
Mahendra Chouhan <mchouhanofficial@gmail.com>
2020-06-14 08:21:43 +0530

    added size param in logs

--- AirServer/src/com/airhockey/handlers/ReadyHandler.java
+++ AirServer/src/com/airhockey/handlers/ReadyHandler.java
@@ -1,6 +1,7 @@
 package com.airhockey.handlers;
 
 import com.airhockey.AirHockeyRoomExtension;
+import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
@@ -11,9 +12,11 @@ public class ReadyHandler extends BaseClientRequestHandler {
     public void handleClientRequest(User user, ISFSObject params) {
 
         AirHockeyRoomExtension airHockeyRoomExtension = (AirHockeyRoomExtension) getParentExtension();
+        Room parentRoom = airHockeyRoomExtension.getParentRoom();
+        Room lastJoinedRoom = user.getLastJoinedRoom();
         trace("[ReadyHandler] Player : " + user.getId()
-                + " parentRoomName : " + airHockeyRoomExtension.getParentRoom().getName()
-                + " lastJoinedroom : " + user.getLastJoinedRoom().getName());
+                + " parentRoomName : " + parentRoom.getName() +","+ parentRoom.getSize()
+                + " lastJoinedroom : " + lastJoinedRoom.getName() + "," + lastJoinedRoom.getSize());
         if(airHockeyRoomExtension.getParentRoom().getUserList().size() == 2)
             airHockeyRoomExtension.startGame();
     }
