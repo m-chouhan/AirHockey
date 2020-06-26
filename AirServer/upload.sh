5a41c4db0b881e2d65ec347c58625fa54b2d14e7
mahendra <mchouhanofficial@gmail.com>
2020-06-26 12:57:57 +0530

    * added upload task in gradle 	* added fatJar task in gradle

--- AirServer/upload.sh
+++ AirServer/upload.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 #simple script to upload our extension to server 
+if [ -z "$1" ]
+then
+      echo "filename is empty"
+      exit 1
+fi
+if [ -z "$2" ]
+then
+      echo "server ip is empty"
+      exit 1
+fi
 
-echo "Uploading AirHockeyExtension.jar to server $1"
+echo "Uploading $1 to server $2"
 
-scp -i ~/.ssh/mumbaiV2.pem ./AirHockeyExtension.jar ec2-user@$1:/home/ec2-user/SmartFoxServer_2X/SFS2X/extensions/AirHockey
+scp -i ~/.ssh/mumbaiV2.pem $1 ec2-user@$2:/home/ec2-user/SmartFoxServer_2X/SFS2X/extensions/AirHockey
 
 returnCode=$?
-echo $returnCode
+echo "scp return code $returnCode"
 exit $returnCode
